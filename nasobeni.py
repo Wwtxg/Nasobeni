edef745785bf00595095f662daf58b40bd37920a
Nikita <nikitamolavko27@gmail.com>
2024-03-11 08:51:02 +0100

    Nas2

--- nasobeni.py
+++ nasobeni.py
@@ -1,36 +1,60 @@
 import random
 
-body = 0
+Pocetbodu = 0
 
 plusbody = 1
 
-def PocetB (body, plusbody):
-    Pocetbodu = body + plusbody
-    return Pocetbodu
+def PocetB (Pocetbodu, plusbody):
+    PocetboduF = Pocetbodu + plusbody
+    return PocetboduF
 
 def nasobenifunkce (x, y):
     nasobeni = x * y
     return nasobeni
 
+def delenifunkce (x, y):
+    deleni = x / y
+    deleni = float
+    return deleni
+
+def minusfunkce (x, y):
+    minus = x - y
+    return minus
+
+def plusfunkce (x, y):
+    plus = x + y
+    return plus
 
 for a in range(9):
     x = random.randint(1, 10)
     y = random.randint(1, 10)
 
-    print (x,"*",y)
+    pmdn = random.choice("-+:*")
+    print (pmdn)
 
-    odpoved = int(input("Zadejte svoji odpoved: "))
+    print (x, pmdn, y)
 
-    Spravna_odpoved = nasobenifunkce(x, y)
+    odpoved = float(input("Zadejte svoji odpoved: "))
+
+    if pmdn == "-":
+        Spravna_odpoved = minusfunkce (x, y)
+    elif pmdn == "+":
+        Spravna_odpoved = plusfunkce (x, y)
+    elif pmdn == "*":
+        Spravna_odpoved = nasobenifunkce (x, y)
+    elif pmdn == "/":
+        Spravna_odpoved = delenifunkce (x, y)
 
     print ("Vase odpoved: ", odpoved)
-    print ("Spravne odpoved: ", Spravna_odpoved)
+    print ("Spravna odpoved: ", Spravna_odpoved)
 
     if odpoved == Spravna_odpoved:
         print ("Spavně")
-        Pocetbodu = PocetB(body, plusbody)
-        print ("Mate", Pocetbodu, "bodu")
+        PocetboduF = PocetB(Pocetbodu, plusbody)
+        Pocetbodu = PocetboduF
+        print ("Mate", PocetboduF, "bodu")
     else:
         print ("Chyba")
-        print ("Mate", Pocetbodu, "bodu")
+        Pocetbodu = PocetboduF
+        print ("Mate", PocetboduF, "bodu")
 
